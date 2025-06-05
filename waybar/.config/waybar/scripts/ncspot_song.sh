dc107f8d212e24f3fe19fcbba4382282fc63dc13
Daniel Sanso <cs.daniel.sanso@gmail.com>
2025-06-05 04:32:22 +0200

    style waybar, add spotify module

--- waybar/.config/waybar/scripts/ncspot_song.sh
+++ waybar/.config/waybar/scripts/ncspot_song.sh
@@ -6,7 +6,7 @@ status=$(playerctl --player="$PLAYER" status 2>/dev/null)
 
 if [[ -z "$status" || "$status" == *"No players found"* ]]; then
     # Player not found, exit with no song.
-    echo '{"text": " No songs currently playing", "class": "stopped"}'
+    echo '{"text": "  No songs currently playing", "class": "stopped"}'
     exit 0
 fi
 
@@ -15,17 +15,22 @@ title=$(playerctl --player="$PLAYER" metadata title 2>/dev/null)
 
 # Truncate if too long
 max_artist_length=15
-max_title_length=25
+max_title_length=20
 
 if [[ ${#artist} -gt $max_artist_length ]]; then
-    artist="${artist:0:$max_artist_length}..."
+    short_artist="${artist:0:$max_artist_length}..."
+else
+    short_artist=$artist
 fi
 
 if [[ ${#title} -gt $max_title_length ]]; then
-    title="${title:0:$max_title_length}..."
+    short_title="${title:0:$max_title_length}..."
+else
+    short_title=$title
 fi
 
+short_display="$short_artist - $short_title"
 display="$artist - $title"
 
 
-echo "{\"text\":\" $display\", \"class\":\"$status\"}"
+echo "{\"text\":\" $short_display\", \"class\":\"$status\", \"tooltip\":\"$display\"}"
