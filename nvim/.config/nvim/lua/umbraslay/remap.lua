792836b2039e279f630c399ba9ba221160c596de
Daniel Sanso <cs.daniel.sanso@gmail.com>
2025-06-07 15:46:25 +0200

    update nvim remaps and add waybar clock module

--- nvim/.config/nvim/lua/umbraslay/remap.lua
+++ nvim/.config/nvim/lua/umbraslay/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {noremap = true})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {noremap = true})
